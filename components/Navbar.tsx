87ddeaa9df0c98fd1336c232bc874e101a26dcb8
unknown <deekayedm@gmail.com>
2026-04-17 15:34:50 +0530

    first commit

---
+++ components/Navbar.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <header className='navbar'>
+      <nav className='inner'>
+        <div className='left'>
+          <div className='brand'></div>
+        </div>
+      </nav>
+    </header> 
+  )
+}
+
+export default Navbar
