d630bfca9dcc7823fc39a7a9e69feff29e3eae0d
unknown <deekayedm@gmail.com>
2026-04-17 15:43:46 +0530

    navbar ui complete

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,14 +1,39 @@
+import { Box } from 'lucide-react'
 import React from 'react'
 
 const Navbar = () => {
+  const handleAuthClick = () => {
+
+  }
   return (
     <header className='navbar'>
       <nav className='inner'>
         <div className='left'>
-          <div className='brand'></div>
+          <div className='brand'>
+            <Box className='logo' />
+            <span className='name'>DreamLand</span>
+          </div>
+          <ul className='links'>
+            <a href='#'>Product</a>
+            <a href='#'>Pricing</a>
+            <a href='#'>Community</a>
+            <a href='#'>Enterprise</a>
+          </ul>
+        </div>
+        <div className='actions'>
+          <button
+            onClick={handleAuthClick}
+            className='login'>
+            Login
+          </button>
+          <a
+            className='cta'
+            href="#upload">
+            Get Started
+          </a>
         </div>
       </nav>
-    </header> 
+    </header>
   )
 }
 
